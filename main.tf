06694fc4d4cf70fc160ff487024be64466f00691
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-21 16:17:42 +0530

    Roboshop terraform

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ variable "components" {
   default = ["frontend", "catalogue", "cart", "user", "shipping", "payment", "dispatch", "mongodb", "mysql", "rabbitmq", "redis"]
 }
 
-resource "aws_instance" "instances" {
+resource "aws_instance" "instance" {
   count                  = length(var.components)
   ami                    = "ami-031da9bb5f8457ab4"
   instance_type          = "t3.small"
