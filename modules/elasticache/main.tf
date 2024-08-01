b174c1b2d26c1edc1ed93a8c3d1c7af9da3036aa
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-08-01 23:12:37 +0530

    Sg change

--- modules/elasticache/main.tf
+++ modules/elasticache/main.tf
@@ -45,6 +45,7 @@ resource "aws_elasticache_cluster" "main" {
   engine_version       = var.engine_version
   port                 = 6379
   subnet_group_name    = aws_elasticache_subnet_group.main.name
+  security_group_ids   = [aws_security_group.main.id]
 
 }
 
