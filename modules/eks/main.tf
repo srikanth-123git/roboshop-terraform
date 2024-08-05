f4fcdd44068888f0fde82f43bc3c96a5b7f0f859
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-08-05 21:08:09 +0530

    Change to memory intensive

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -45,7 +45,7 @@ resource "aws_eks_node_group" "main" {
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_ids
   capacity_type   = "SPOT"
-  instance_types  = ["t3.large"]
+  instance_types  = ["r7i.large"]
 
   launch_template {
     name    = "eks-${var.env}"
